b1f81bd0c8dcfc9326a43e80d0a8427f7354cfe3
Larry <larry.laski@gmail.com>
2017-09-11 13:04:36 -0400

    Initial commit

---
+++ src/NewCommand.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App;
+
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use ZipArchive;
+
+class NewCommand extends Command {
+    private $client;
+
+    public function __construct(ClientInterface $client)
+    {
+        $this->client = $client;
+
+        parent::__construct();
+    }
+
+
+    public function configure()
+    {
+        $this->setName('new')
+            ->setDescription('Create a new laravel application.')
+            ->addArgument('name', InputArgument::REQUIRED);
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $directory = getcwd() . '/' . $input->getArgument('name');
+        $this->assertApplicationDoesNotExist($directory, $output);
+
+        $this->download($zipFile = $this->makeFileName())
+            ->extract($zipFile, $directory)
+            ->cleanUp($zipFile);
+
+        $output->writeln('<comment>Application ready!</comment>');
+    }
+
+    private function assertApplicationDoesNotExist($directory, OutputInterface $output)
+    {
+        if (is_dir($directory)) {
+            $output->writeln('<error>Application already exists</error>');
+            exit(1);
+        }
+    }
+
+    private function makeFileName()
+    {
+        return getcwd() . '/laravel_' . md5(time().uniqid()) . '.zip';
+    }
+
+    private function download($zipFile)
+    {
+        $response = $this->client->get('http://cabinet.laravel.com/latest.zip')->getBody();
+        file_put_contents($zipFile, $response);
+
+        return $this;
+    }
+
+    private function extract($zipFile, $directory)
+    {
+        $archive = new ZipArchive;
+
+        $archive->open($zipFile);
+        $archive->extractTo($directory);
+        $archive->close();
+
+        return $this;
+    }
+
+    private function cleanUp($zipFile)
+    {
+        @chmod($zip, 0777);
+        @unlink($zipFile);
+
+        return $this;
+    }
+}
